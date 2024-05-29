a067b1778622499f468b43851e60d865c85a6464
Valera-ПК\Антон <555z.anton@gmail.com>
2024-05-29 19:16:15 +0600

    + repair ALL + screens

---
+++ app/src/main/java/com/example/marvelapp/network/ResponseStatus.kt
@@ -0,0 +1,5 @@
+package com.example.marvelapp.network
+
+enum class ResponseStatus {
+    LOADING, ERROR, DONE
+}
---
+++ app/src/main/java/com/example/marvelapp/network/RetrofitService.kt
@@ -0,0 +1,62 @@
+package com.example.marvelapp.network
+
+import android.util.Log
+import com.example.marvelapp.network.api.MarvelApi
+import com.example.marvelapp.network.rep.CatalogRep
+import com.example.marvelapp.network.rep.CatalogRepI
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RetrofitService {
+
+    @Provides
+    fun provideOkHttpClient(constants: Constants): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(createLoggingInterceptor())
+            .addInterceptor(constants)
+            .build()
+    }
+
+    private fun createLoggingInterceptor(): HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor.Logger { message ->
+            Log.d("OkHttp", message)
+        }
+        val loggingInterceptor = HttpLoggingInterceptor(logger)
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        return Retrofit.Builder()
+            .baseUrl("https://gateway.marvel.com/")
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCatalogRep(api: MarvelApi): CatalogRep {
+        return CatalogRepI(api)
+    }
+
+    @Provides
+    fun provideMarvelApi(retrofit: Retrofit): MarvelApi {
+        return retrofit.create(MarvelApi::class.java)
+    }
+}
